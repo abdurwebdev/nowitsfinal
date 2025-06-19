68177aed39529e5866233765de6db2ecd0b2d351
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-06-19 14:42:37 +0500

    ggg

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -32,6 +34,7 @@ const Login = () => {
       <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
+      <p>Don't have an account? <Link to="/register">Register</Link></p>
     </div>
   );
 };
